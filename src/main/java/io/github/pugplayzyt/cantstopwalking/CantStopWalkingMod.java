7987794b41b13dc1c4a12015726252b2a4b7e689
pugplayzYT <pugplayzthesecond@gmail.com>
2026-08-30 12:22:57 +0930

    Add Forge mod entry point

---
+++ src/main/java/io/github/pugplayzyt/cantstopwalking/CantStopWalkingMod.java
@@ -0,0 +1,8 @@
+package io.github.pugplayzyt.cantstopwalking;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CantStopWalkingMod.MOD_ID)
+public final class CantStopWalkingMod {
+    public static final String MOD_ID = "cantstopwalking";
+}
