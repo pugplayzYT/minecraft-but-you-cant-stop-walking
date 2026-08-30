ed207b6262e6133eaf48dade24a3d0fde5c008dc
pugplayzYT <pugplayzthesecond@gmail.com>
2026-08-30 12:23:08 +0930

    Implement forced walking and jumping challenge

---
+++ src/main/java/io/github/pugplayzyt/cantstopwalking/ClientChallengeEvents.java
@@ -0,0 +1,124 @@
+package io.github.pugplayzyt.cantstopwalking;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = CantStopWalkingMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public final class ClientChallengeEvents {
+    private static boolean forceWalking = false;
+    private static boolean forceJumping = false;
+    private static boolean wasInWorld = false;
+
+    private ClientChallengeEvents() {
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent.Pre event) {
+        Minecraft minecraft = Minecraft.getInstance();
+        boolean inWorld = minecraft.player != null && minecraft.level != null;
+
+        if (inWorld && !wasInWorld) {
+            minecraft.setScreen(new ChallengeScreen());
+        }
+
+        if (inWorld) {
+            applyForcedControls(minecraft);
+        } else if (wasInWorld) {
+            releaseForcedControls(minecraft);
+        }
+
+        wasInWorld = inWorld;
+    }
+
+    private static void applyForcedControls(Minecraft minecraft) {
+        if (forceWalking) {
+            // W is always held. Every other movement direction is hard-disabled,
+            // so S/A/D cannot cancel or steer against the challenge.
+            minecraft.options.keyUp.setDown(true);
+            minecraft.options.keyDown.setDown(false);
+            minecraft.options.keyLeft.setDown(false);
+            minecraft.options.keyRight.setDown(false);
+            minecraft.options.keySprint.setDown(false);
+            minecraft.options.keyShift.setDown(false);
+            minecraft.options.keyJump.setDown(forceJumping);
+            return;
+        }
+
+        if (forceJumping) {
+            minecraft.options.keyJump.setDown(true);
+        }
+    }
+
+    private static void releaseForcedControls(Minecraft minecraft) {
+        minecraft.options.keyUp.setDown(false);
+        minecraft.options.keyDown.setDown(false);
+        minecraft.options.keyLeft.setDown(false);
+        minecraft.options.keyRight.setDown(false);
+        minecraft.options.keySprint.setDown(false);
+        minecraft.options.keyShift.setDown(false);
+        minecraft.options.keyJump.setDown(false);
+    }
+
+    private static final class ChallengeScreen extends Screen {
+        private Button walkingButton;
+        private Button jumpingButton;
+
+        private ChallengeScreen() {
+            super(Component.literal("What do you want? Do you want Minecraft?"));
+        }
+
+        @Override
+        protected void init() {
+            int centerX = this.width / 2;
+            int startY = this.height / 2 - 35;
+
+            this.walkingButton = this.addRenderableWidget(Button.builder(
+                    walkingText(),
+                    button -> {
+                        forceWalking = !forceWalking;
+                        button.setMessage(walkingText());
+                    }
+            ).bounds(centerX - 120, startY, 240, 20).build());
+
+            this.jumpingButton = this.addRenderableWidget(Button.builder(
+                    jumpingText(),
+                    button -> {
+                        forceJumping = !forceJumping;
+                        button.setMessage(jumpingText());
+                    }
+            ).bounds(centerX - 120, startY + 28, 240, 20).build());
+
+            this.addRenderableWidget(Button.builder(
+                    Component.literal("Start"),
+                    button -> this.onClose()
+            ).bounds(centerX - 60, startY + 65, 120, 20).build());
+        }
+
+        private Component walkingText() {
+            return Component.literal((forceWalking ? "[x] " : "[ ] ") + "You can't stop walking");
+        }
+
+        private Component jumpingText() {
+            return Component.literal((forceJumping ? "[x] " : "[ ] ") + "You can't stop jumping");
+        }
+
+        @Override
+        public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+            this.renderBackground(graphics, mouseX, mouseY, partialTick);
+            graphics.drawCenteredString(this.font, this.title, this.width / 2, this.height / 2 - 70, 0xFFFFFF);
+            super.render(graphics, mouseX, mouseY, partialTick);
+        }
+
+        @Override
+        public boolean isPauseScreen() {
+            return false;
+        }
+    }
+}
