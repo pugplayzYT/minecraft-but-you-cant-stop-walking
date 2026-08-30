8d1e85c64f96d37fccaf631a1a01bb311f2f698b
pugplayzYT <pugplayzthesecond@gmail.com>
2026-08-30 12:38:43 +0930

    Port mod entrypoint to Fabric

--- src/main/java/io/github/pugplayzyt/cantstopwalking/CantStopWalkingMod.java
+++ src/main/java/io/github/pugplayzyt/cantstopwalking/CantStopWalkingMod.java
@@ -1,8 +1,13 @@
 package io.github.pugplayzyt.cantstopwalking;
 
-import net.minecraftforge.fml.common.Mod;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
-@Mod(CantStopWalkingMod.MOD_ID)
-public final class CantStopWalkingMod {
+public final class CantStopWalkingMod implements ClientModInitializer {
     public static final String MOD_ID = "cantstopwalking";
+
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.START_CLIENT_TICK.register(ClientChallengeEvents::onClientTick);
+    }
 }
